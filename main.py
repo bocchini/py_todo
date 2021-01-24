f49c24d21123be336768752caeb9d9368ab71590
Américo L Bocchini <albocchini@gmail.com>
2021-01-24 13:25:22 -0300

    feat: screen start and initial structute

---
+++ main.py
@@ -0,0 +1,15 @@
+from view.menu import start_screen
+
+while True:
+    start_screen()
+    option = input('Digite um opção: ')
+    if option.lower() == 'a':
+        pass
+    elif option.lower() == 'd':
+        pass
+    elif option.lower() == 'f':
+        pass
+    elif option.lower() == 'd':
+        pass
+    elif option.lower() == 's':
+        break
---
+++ view/menu.py
@@ -0,0 +1,9 @@
+def start_screen():
+    print('*' * 100)
+    print('*' * 100)
+    print('Para adicionar uma tarefa digite: a')
+    print('Para listar uma tarefa digite: l')
+    print('Para desfazer uma tarefa digite: d')
+    print('Para refazer uma tarefa digite: r')
+    print('Para sair digite s')
+    print('*' * 100)
